e14c2338e06a58ca54b04906be54d6a09fd6db08
Artemij Ivshin <sgds.2020.nag@yandex.ru>
2022-01-13 20:38:02 +0500

    Удалил не используемые в решении проекты.

--- Extensions/StickerSetExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Extensions
-{
-    public static class StickerSetExtensions
-    {
-        public 
-    }
-}
--- TaskQueue/TaskQueue.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TaskQueue
-{
-    public class TaskQueue
-    {
-    }
-}
